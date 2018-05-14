032fac6aa136c769ce95b5cc84916c2c0c829339
dohien <dohien@Admins-iMac-2.local>
2018-05-14 08:10:13 +0700

    Initial Commit

---
+++ Autolayout2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Autolayout2
+//
+//  Created by dohien on 5/14/18.
+//  Copyright © 2018 dohien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
